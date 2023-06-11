1aea892aadff0441ed2115b84da0c543d0430085
prithvi5481 <prithvisingh91827@gmail.com>
2023-06-11 19:56:29 +0530

    project completed

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import Editor from './Editor'
+import {Box,styled} from '@mui/material';
+import { DataContext } from '../context/DataProvider';
 
+
+const Container =  styled(Box)`
+    display: flex;
+    background-color: #060606;
+    height:50vh
+`
 const Code = () => {
+  const {html,setHtml,css,setCss,js,setJs} = useContext(DataContext)
   return (
-    <div>
-        <Editor heading="HTML" icon='/' color='#FF3C41' />
-        <Editor heading="CSS" icon='*' color='#0EBEFF'/>
-        <Editor heading="JavaScript" icon='()' color='#FCD000'/>
-    </div>
+    <Container>
+        <Editor 
+          heading="HTML" 
+          icon='/' 
+          color='#FF3C41' 
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor 
+          heading="CSS" 
+          icon='*' 
+          color='#0EBEFF'
+          value={css}
+          onChange={setCss}
+        />
+        <Editor 
+          heading="JavaScript" 
+          icon='()' 
+          color='#FCD000'
+          value={js}
+          onChange={setJs}
+        />
+    </Container>
   )
 }
 
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Box,styled} from '@mui/material'
 import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
 import {Controlled as ControlledEditor} from 'react-codemirror2';
@@ -10,6 +10,14 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/css/css';
 
 import '../App.css'
+
+const Container = styled(Box)`
+    flex-grow: 1;
+    flex-basic: 0;
+    display: flex;
+    flex-direction: column;
+    padding: 0px 8px 8px;
+`
 
 const Heading = styled(Box)`
     background: #1d1e22;
@@ -24,8 +32,13 @@ const Header = styled(Box)`
     font-weight: 700
 `
 
-const Editor = ({heading,icon,color}) => (
-  <Box>
+const Editor = ({heading,icon,color,value,onChange}) => {
+  const [open,setOpen] = useState(true);
+  const handleChange = (editor,data,value) => {
+    onChange(value)
+  }
+  return(
+  <Container style={open?null:{flexGrow:0}}>
     <Header>
       <Heading>
         <Box
@@ -44,15 +57,21 @@ const Editor = ({heading,icon,color}) => (
         >{icon}</Box>
         {heading}
       </Heading>
-      <CloseFullscreenIcon />
+      <CloseFullscreenIcon 
+          fontSize='small'
+          style={{alignSelf:'center'}}
+          onClick={()=>setOpen(preState=> !preState)}
+      />
     </Header>
     <ControlledEditor
       className='controlled-editor'
+      value = {value}
+      onBeforeChange={handleChange}
       options={{
         theme: 'material',
         lineNumbers: true
       }} />
-  </Box>
-)
+  </Container>
+)}
 
 export default Editor
